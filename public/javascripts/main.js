afe87df1e1414f70be3a697c2b9436a71f82fac3
Ruoran Liu <rrliu@thoughtworks.com>
2013-11-15 14:03:34 +0800

    Dynamically display monitor adding section

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,25 @@
 $(function(){
-  $('.show-monitor').click(function(){
-    $('.modal.add-monitor').modal('show');
-  });
-  $('.modal.add-monitor .close').click(function(){
-    $('.modal.add-monitor').modal('hide');
-  });
-  $('.show-group').click(function(){
-    $('.modal.add-group').modal('show');
-  });
-  $('.modal.add-group .close').click(function(){
-    $('.modal.add-group').modal('hide');
-  });
+  bindModal('.show-monitor', '.modal.add-monitor');
+  bindModal('.show-group', '.modal.add-group');
+
+  selectMonitorType();
+  $('.add-monitor .type').change(function(){
+    selectMonitorType();
+  })
 });
+
+function bindModal(triggerButtonSelector, modalSelector){
+  $(triggerButtonSelector).click(function(){
+    $(modalSelector).modal('show');
+  });
+  $(modalSelector + ' .close').click(function(){
+    $(modalSelector).modal('hide')
+  })
+}
+
+function selectMonitorType(){
+  var type = $('.add-monitor .type').val();
+  var className = ".add-" + type.toLowerCase().replace(/\s/g, '-');
+  $('.add-monitor .monitor-section').css('display', 'none');
+  $('.add-monitor ' + className).css('display', 'block');
+}
